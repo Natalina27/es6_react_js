a02e09fa43f4b2f7012ca6ec547a577eb2311416
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-03 15:47:35 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const getMovie = function (name, start) {
+  return name + ' , ' + start;
+};
+console.log( 'Function expression getMovie return name + start :  ' , getMovie('Averages', '2018'));
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='wrap'>
+        <p className='wrap__title'>1. Arrow functions</p>
+      </div>
   );
 }
 
