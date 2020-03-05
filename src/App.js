56912d330eb595f38fbc93ffa17063e54bbf63ce
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-05 16:24:14 +0100

    Arrow functions, this context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
 
-const getMovie = function (name, start) {
-  return name + ' , ' + start;
-};
+//1.
+const getMovie = (name, start) => name + ' , ' + start;
+//const getMovie = function(name, start){return name + ' , ' + start;}
 console.log( 'Function expression getMovie return name + start :  ' , getMovie('Averages', '2018'));
+
+//2.
+
+
+// function API () {
+//     this.url = 'https://reactwarriors.com';
+// }
+// API.prototype.get = function () {
+//     return [1, 2].map(function (number) {
+//         console.log('this', this);
+//         return number;
+//     }.bind(this))
+// }
+// const api = new API();
+// api.get();
+
+
+
 function App() {
   return (
       <div className='wrap'>
